55c868265f0823c16e3f91f72118966ef074cc8b
Cristian Ramirez <cristiancrc97@hotmail.com>
2022-06-29 16:28:54 -0500

    onClick listener added to the itemViews to show the hero name

--- app/src/main/java/com/cristiandev/recyclerview_practice/MainActivity.kt
+++ app/src/main/java/com/cristiandev/recyclerview_practice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.cristiandev.recyclerview_practice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.cristiandev.recyclerview_practice.databinding.ActivityMainBinding
 import com.cristiandev.recyclerview_practice.recycler.adapter.SuperHeroAdapter
@@ -19,6 +20,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun initRecyclerView(){
         binding.recyclerSuperHero.layoutManager = LinearLayoutManager(this)
-        binding.recyclerSuperHero.adapter = SuperHeroAdapter(SuperHeroProvider.superHeroList)
+        binding.recyclerSuperHero.adapter =
+            SuperHeroAdapter(SuperHeroProvider.superHeroList) { superHero ->
+                onItemSelected(superHero)
+            }
+    }
+
+    private fun onItemSelected(superHero: SuperHero) {
+        Toast.makeText(this, superHero.heroName, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/cristiandev/recyclerview_practice/recycler/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/cristiandev/recyclerview_practice/recycler/adapter/SuperHeroViewHolder.kt
@@ -3,19 +3,19 @@ package com.cristiandev.recyclerview_practice.recycler.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
 import com.cristiandev.recyclerview_practice.SuperHero
 import com.cristiandev.recyclerview_practice.databinding.ItemSuperheroBinding
 
 class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     private val binding = ItemSuperheroBinding.bind(view)
-    fun render(superHero: SuperHero) {
+    fun render(superHero: SuperHero, onClickListener: (SuperHero) -> Unit) {
         binding.tvSuperHeroName.text = superHero.heroName
         binding.tvRealName.text = superHero.realName
         binding.tvPublisher.text = superHero.publisher
         Glide.with(binding.ivSuperHero.context)
             .load(superHero.photo)
             .into(binding.ivSuperHero)
+        itemView.setOnClickListener { onClickListener(superHero) }
     }
 }
