fea4bdbf6cfc155f20de5ae1d90d9faa31717385
Cristian Ramirez <cristiancrc97@hotmail.com>
2022-06-29 15:50:18 -0500

    Hero provider info updated

--- app/src/main/java/com/cristiandev/recyclerview_practice/SuperHeroProvider.kt
+++ app/src/main/java/com/cristiandev/recyclerview_practice/SuperHeroProvider.kt
@@ -6,38 +6,38 @@ class SuperHeroProvider {
             SuperHero(
                 "Batman",
                 "Bruce Wayne",
-                "DC Comics",
-                "https://www.quever.news/u/fotografias/m/2022/2/25/f848x477-24647_82450_5119.jpg"
+                "DC",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/batman.jpg"
             ),
             SuperHero(
-                "Superman",
-                "Clark Kent",
-                "DC Comics",
-                "https://i.blogs.es/fea2de/henry-cavill-superman/450_1000.jpeg"
+                "Spiderman",
+                "Peter Parker",
+                "DC",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/spiderman.jpg"
             ),
             SuperHero(
                 "Thor",
                 "Thor",
                 "Marvel",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMNJd_5aLYuGCO63PtspHIGGxFbr08uP6TBtkKDYhs-EkyQ4BxGT0lENqaHF0ACkT044M&usqp=CAU"
+                "https://cursokotlin.com/wp-content/uploads/2017/07/thor.jpg"
             ),
             SuperHero(
-                "IronMan",
-                "Tony Stark",
+                "Wonder Woman",
+                "Diana",
                 "Marvel",
-                "https://sm.ign.com/t/ign_es/screenshot/default/iron-man-marvel-regreso_hft3.1200.jpg"
+                "https://cursokotlin.com/wp-content/uploads/2017/07/wonder_woman.jpg"
             ),
             SuperHero(
-                "Hulk",
-                "Bruce Banner",
+                "Wolverine",
+                "Logan",
                 "Marvel",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSB1zH9pc-5_Gg2OSqxnunc7Og5GFT3G0GcgCVeORJV0vPvUkWfPkcxU9qcKsvqn__z5I4&usqp=CAU"
+                "https://cursokotlin.com/wp-content/uploads/2017/07/logan.jpeg"
             ),
             SuperHero(
-                "Capitan America",
-                "Rogers",
-                "Marvel",
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFAx0jC17hjjhcKdu_GSV9-5kI43Tn72xXDZSB-ohfif8KX_9IHV9--gNeKGYr9yNVy7U&usqp=CAU"
+                "Green Lantern",
+                "Alan Scott",
+                "DC",
+                "https://cursokotlin.com/wp-content/uploads/2017/07/green-lantern.jpg"
             ),
         )
     }
--- app/src/main/java/com/cristiandev/recyclerview_practice/recycler/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/cristiandev/recyclerview_practice/recycler/adapter/SuperHeroViewHolder.kt
@@ -3,6 +3,7 @@ package com.cristiandev.recyclerview_practice.recycler.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.cristiandev.recyclerview_practice.SuperHero
 import com.cristiandev.recyclerview_practice.databinding.ItemSuperheroBinding
 
@@ -13,6 +14,8 @@ class SuperHeroViewHolder(view: View): RecyclerView.ViewHolder(view) {
         binding.tvSuperHeroName.text = superHero.heroName
         binding.tvRealName.text = superHero.realName
         binding.tvPublisher.text = superHero.publisher
-        Glide.with(binding.ivSuperHero.context).load(superHero.photo).into(binding.ivSuperHero)
+        Glide.with(binding.ivSuperHero.context)
+            .load(superHero.photo)
+            .into(binding.ivSuperHero)
     }
 }
